ba953e5378dc3b427f4eab37099865f1132deb96
Aakash260 <akashnirwan26@gmail.com>
2025-08-25 14:42:33 +0530

    initialize-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
-  return <div>Rect App</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/auth/login");
+  }, [navigate]);
+
+  return <div>React App</div>;
 };
 
 export default App;
