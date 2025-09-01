05b32cf982e62dae69c82ada81e64dbce881321d
akashNirwan <aakash.nirwan@vayuz.com>
2025-09-01 12:34:08 +0530

    add crousel screen

--- src/pages/MicroExperience.jsx
+++ src/pages/MicroExperience.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Header from "../components/level/header";
 import Question from "../components/level/question";
 import LevelCarousel from "../components/level/crousel";
-import Button from "../components/level/button";
+import Button from "../components/common/Button";
+
 const Level = () => {
   return (
     <div className="bg-[#130934] min-h-screen overflow-x-hidden">
       <Header />
       <Question />
       <LevelCarousel />
-      <Button />
+      {/* <Button /> */}
     </div>
   );
 };
