69937f6d6a7b2212eddf6286f2306ce2b82745b0
akashNirwan <aakash.nirwan@vayuz.com>
2025-09-16 22:03:56 +0530

    message

--- src/router/onboardWrapper/index.jsx
+++ src/router/onboardWrapper/index.jsx
@@ -91,7 +91,7 @@ export default function OnBoardWrapper() {
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.4 }}
-          className="fixed bottom-0 left-0 right-0 mx-auto w-full max-w-[425px] flex flex-col gap-5 p-6 h-[60vh] z-20 "
+          className="fixed bottom-0 left-0 right-0 mx-auto w-full max-w-[425px] flex flex-col gap-5 p-6 h-[62vh] z-20 "
           style={{
             borderRadius: "24px 24px 0 0",
             background:
