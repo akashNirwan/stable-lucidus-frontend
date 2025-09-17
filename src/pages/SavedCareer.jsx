f8e6144a909ee4d95f1b713d9fd6ef728bb6db9e
akashNirwan <aakash.nirwan@vayuz.com>
2025-09-17 17:25:57 +0530

    updated code

--- src/pages/SavedCareer.jsx
+++ src/pages/SavedCareer.jsx
@@ -1,29 +1,36 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CareerExperienceCard from "./ExperienceCard";
 import { fetchSavedCareers } from "../redux/actions/dashboard-action";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingSpinner from "../components/common/LoadingSpinner";
+import { useEffect } from "react";
 
 const SavedCareer = () => {
   const dispatch = useDispatch();
+
   const {
-    fetchsavedCareer = [],
+    fetchsavedCareer,
     savedCareerLoading,
     loadMoreLoading,
     hasMoreCareers,
     currentPage,
+    totalCareers,
+    error,
   } = useSelector((state) => state.dashboard);
+
+  console.log(fetchsavedCareer, "fetch save data");
 
   useEffect(() => {
     dispatch(fetchSavedCareers({ page: 1 }));
   }, [dispatch]);
 
   const handleScroll = (e) => {
-    const isBottom =
-      e.target.scrollHeight - e.target.scrollTop <= e.target.clientHeight + 2;
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
 
-    if (isBottom && hasMoreCareers && !loadMoreLoading && !savedCareerLoading) {
+    if (bottom && hasMoreCareers && !loadMoreLoading && !savedCareerLoading) {
       const nextPage = currentPage + 1;
+      console.log(`Loading page ${nextPage}`);
       dispatch(fetchSavedCareers({ page: nextPage }));
     }
   };
@@ -35,15 +42,14 @@ const SavedCareer = () => {
       </div>
     );
   }
-
   return (
     <div
-      className="overflow-y-auto mb-20 border"
-      style={{ maxHeight: "calc(100vh - 100px)" }}
+      className="h-[100dvh] overflow-y-auto  mb-20"
       onScroll={handleScroll}
+      style={{ maxHeight: "calc(100vh - 100px)" }}
     >
-      <div className="grid gap-4 p-4">
-        {fetchsavedCareer.length > 0 ? (
+      <div className="grid gap-4 p-4 ">
+        {fetchsavedCareer && fetchsavedCareer.length > 0 ? (
           fetchsavedCareer.map((savedCareer) => (
             <CareerExperienceCard
               key={savedCareer._id}
@@ -56,15 +62,41 @@ const SavedCareer = () => {
           <p className="text-center text-gray-500">No saved careers found</p>
         )}
 
-        {(loadMoreLoading || savedCareerLoading) && (
-          <div className="flex items-center justify-center py-4">
-            <LoadingSpinner size={32} />
-            <span className="ml-2 text-gray-600">Loading more...</span>
-          </div>
-        )}
+        {loadMoreLoading ||
+          (savedCareerLoading && (
+            <div className="flex items-center justify-center py-4">
+              <LoadingSpinner size={32} />
+              <span className="ml-2 text-gray-600">Loading more...</span>
+            </div>
+          ))}
       </div>
     </div>
   );
 };
 
 export default SavedCareer;
+
+//   return savedCareerLoading?  (
+//       <div className="flex items-center justify-center min-h-[400px]">
+//             <LoadingSpinner size={64} />
+//           </div>
+//   ) : (
+
+//     <div className="grid gap-4 p-4">
+//   {fetchsavedCareer && fetchsavedCareer.length > 0 ? (
+//     fetchsavedCareer.map((savedCareer) => (
+//       <CareerExperienceCard
+//         key={savedCareer._id}
+//         careerData={savedCareer.careers[0]}
+//         savedCareer={savedCareer}
+//         careerId={savedCareer.careerId}
+//       />
+//     ))
+//   ) : (
+//     <p className="text-center text-gray-500">No data available</p>
+//   )}
+// </div>
+//   )
+// };
+
+// export default SavedCareer;
