59e4af302dda981067431a7cfea99907dd401493
akashNirwan <aakash.nirwan@vayuz.com>
2025-09-17 16:06:49 +0530

    updated code

--- src/pages/DashBoardMicro.jsx
+++ src/pages/DashBoardMicro.jsx
@@ -6,35 +6,40 @@ import LoadingSpinner from "../components/common/LoadingSpinner";
 import { useDispatch, useSelector } from "react-redux";
 
 const DashBoardMicro = () => {
-  
-
-
-
   const dispatch = useDispatch();
-  const { 
-    dashboardMicroexperience, 
-    dashboardMicroexperienceLoading, 
+  const {
+    dashboardMicroexperience,
+    dashboardMicroexperienceLoading,
     loadMoreLoading,
     hasMoreCareers,
     currentPage,
-    error 
+    error,
   } = useSelector((state) => state.dashboard);
 
   useEffect(() => {
     dispatch(fetchDashboardMicroexperience({ page: 1 }));
   }, [dispatch]);
 
-const handleScroll = (e) => {
-    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-    
-    if (bottom && hasMoreCareers && !loadMoreLoading && !dashboardMicroexperienceLoading) {
+  const handleScroll = (e) => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+
+    if (
+      bottom &&
+      hasMoreCareers &&
+      !loadMoreLoading &&
+      !dashboardMicroexperienceLoading
+    ) {
       const nextPage = currentPage + 1;
       console.log(`Loading page ${nextPage}`);
       dispatch(fetchDashboardMicroexperience({ page: nextPage }));
     }
   };
 
-  if (dashboardMicroexperienceLoading && (!dashboardMicroexperience || dashboardMicroexperience.length === 0)) {
+  if (
+    dashboardMicroexperienceLoading &&
+    (!dashboardMicroexperience || dashboardMicroexperience.length === 0)
+  ) {
     return (
       <div className="flex items-center justify-center min-h-[400px]">
         <LoadingSpinner size={64} />
@@ -42,27 +47,24 @@ const handleScroll = (e) => {
     );
   }
 
-
-
-     return (
-    <div 
+  return (
+    <div
       className="h-full overflow-y-auto"
       onScroll={handleScroll}
-      style={{ maxHeight: 'calc(100vh - 100px)' }}
+      style={{ maxHeight: "calc(100vh - 100px)" }}
     >
-      <div className="grid gap-4 p-4">
+      <div className="grid gap-4 p-4  mb-18">
         {dashboardMicroexperience && dashboardMicroexperience.length > 0 ? (
           dashboardMicroexperience.map((item) => (
-            <CareerCard 
-              key={item._id}
-              data={item}
-            />
+            <CareerCard key={item._id} data={item} />
           ))
         ) : (
-          <p className="text-center text-gray-500">No micro experiences found</p>
+          <p className="text-center text-gray-500">
+            No micro experiences found
+          </p>
         )}
 
-        {loadMoreLoading  && (
+        {loadMoreLoading && (
           <div className="flex items-center justify-center py-4">
             <LoadingSpinner size={32} />
             <span className="ml-2 text-gray-600">Loading more...</span>
@@ -80,12 +82,12 @@ export default DashBoardMicro;
 //      <div className="flex items-center justify-center min-h-[400px]">
 //           <LoadingSpinner size={64} />
 //         </div>
-    
+
 //   ) : (
 
 //       <div className="space-y-4 h-[520px] overflow-y-auto">
 //       {dashboardMicroexperience?.map((item) => (
-//         <CareerCard 
+//         <CareerCard
 //           key={item._id}
 //           data={item}
 //         />
--- src/pages/SavedCareer.jsx
+++ src/pages/SavedCareer.jsx
@@ -6,26 +6,28 @@ import LoadingSpinner from "../components/common/LoadingSpinner";
 import { useEffect } from "react";
 
 const SavedCareer = () => {
-
   const dispatch = useDispatch();
-  
-  const {fetchsavedCareer ,  savedCareerLoading,loadMoreLoading,
+
+  const {
+    fetchsavedCareer,
+    savedCareerLoading,
+    loadMoreLoading,
     hasMoreCareers,
     currentPage,
-    totalCareers, error } = useSelector(
-    (state) => state.dashboard
-  );
-    
-  console.log(fetchsavedCareer,"fetch save data");
-  
-  
+    totalCareers,
+    error,
+  } = useSelector((state) => state.dashboard);
+
+  console.log(fetchsavedCareer, "fetch save data");
+
   useEffect(() => {
     dispatch(fetchSavedCareers({ page: 1 }));
   }, [dispatch]);
 
-const handleScroll = (e) => {
-    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-    
+  const handleScroll = (e) => {
+    const bottom =
+      e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+
     if (bottom && hasMoreCareers && !loadMoreLoading && !savedCareerLoading) {
       const nextPage = currentPage + 1;
       console.log(`Loading page ${nextPage}`);
@@ -33,7 +35,7 @@ const handleScroll = (e) => {
     }
   };
 
-   if (savedCareerLoading && fetchsavedCareer.length === 0) {
+  if (savedCareerLoading && fetchsavedCareer.length === 0) {
     return (
       <div className="flex items-center justify-center min-h-[400px]">
         <LoadingSpinner size={64} />
@@ -41,14 +43,13 @@ const handleScroll = (e) => {
     );
   }
 
-
-  return   (
-    <div 
+  return (
+    <div
       className="h-full overflow-y-auto"
       onScroll={handleScroll}
-      style={{ maxHeight: 'calc(100vh - 100px)' }}
+      style={{ maxHeight: "calc(100vh - 100px)" }}
     >
-      <div className="grid gap-4 p-4">
+      <div className="grid gap-4 p-4  mb-18">
         {fetchsavedCareer && fetchsavedCareer.length > 0 ? (
           fetchsavedCareer.map((savedCareer) => (
             <CareerExperienceCard
@@ -61,18 +62,14 @@ const handleScroll = (e) => {
         ) : (
           <p className="text-center text-gray-500">No saved careers found</p>
         )}
-        
-        
-        {loadMoreLoading || savedCareerLoading && (
-          <div className="flex items-center justify-center py-4">
-            <LoadingSpinner size={32} />
-            <span className="ml-2 text-gray-600">Loading more...</span>
-          </div>
-        )}
-        
-        
-        
-       
+
+        {loadMoreLoading ||
+          (savedCareerLoading && (
+            <div className="flex items-center justify-center py-4">
+              <LoadingSpinner size={32} />
+              <span className="ml-2 text-gray-600">Loading more...</span>
+            </div>
+          ))}
       </div>
     </div>
   );
@@ -80,14 +77,12 @@ const handleScroll = (e) => {
 
 export default SavedCareer;
 
-
-
 //   return savedCareerLoading?  (
 //       <div className="flex items-center justify-center min-h-[400px]">
 //             <LoadingSpinner size={64} />
 //           </div>
 //   ) : (
-         
+
 //     <div className="grid gap-4 p-4">
 //   {fetchsavedCareer && fetchsavedCareer.length > 0 ? (
 //     fetchsavedCareer.map((savedCareer) => (
