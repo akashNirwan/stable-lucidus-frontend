a40963e0c7ff30dc21f82a19b23a10e882c9d064
akashNirwan <aakash.nirwan@vayuz.com>
2025-09-17 18:17:51 +0530

    updated code

--- src/router/onboardWrapper/index.jsx
+++ src/router/onboardWrapper/index.jsx
@@ -46,12 +46,7 @@ export default function OnBoardWrapper() {
         </div>
 
         <div className="relative z-10 flex flex-col items-center justify-start h-screen px-4 pt-12">
-          <img
-            src="/onBoard-rob2.png"
-            alt="Astronaut"
-            width={300}
-            height={300}
-          />
+          <img src="/Astronaut2.svg" alt="Astronaut" />
         </div>
 
         <div className="absolute top-4 left-1/2 -translate-x-1/2 w-full max-w-[375px] px-4">
