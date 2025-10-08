012950deed75e04c560e61fc2002aec5fbdd8834
akashNirwan <aakash.nirwan@vayuz.com>
2025-10-08 12:24:57 +0530

    udpated code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bl3j4mrgd7"
+    "revision": "0.3fbuul6u5ho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
