05fd4be3ec1d72b5fbb337c28271cadfbea54d2c
akashNirwan <aakash.nirwan@vayuz.com>
2025-10-15 15:02:11 +0530

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,8 @@ import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
 
-import { SpeedInsights } from "@vercel/speed-insights/next";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <SpeedInsights />
     <Toaster></Toaster>
     <RouterConfiguration />
   </Provider>
