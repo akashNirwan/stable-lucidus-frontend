87231886fbb47512505097733df33d52c9c39a28
akashNirwan <aakash.nirwan@vayuz.com>
2025-10-17 10:53:15 +0530

    updated code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8k74dljl6kg"
+    "revision": "0.bvtthqlbgbo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
